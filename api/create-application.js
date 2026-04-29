0a04340958310f2a91ea6c50d62ed736dade168f
developer-orhc <dpo.orhc@gmail.com>
2026-04-29 17:44:54 +0100

    Add files via upload

---
+++ api/create-application.js
@@ -0,0 +1,48 @@
+export default async function handler(req, res) {
+  try {
+    const isLive = process.env.DOPPLE_ENV === "prod";
+
+    const endpoint = isLive
+      ? "https://app.dopplepay.com/api/merchants/applications"
+      : "https://uat-app.dopplepay.com/api/merchants/applications";
+
+    const identifier = `optimum-${Date.now()}`;
+
+    const response = await fetch(endpoint, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${process.env.DOPPLE_API_KEY}`
+      },
+      body: JSON.stringify({
+        system_id: process.env.DOPPLE_SYSTEM_ID,
+        identifier: identifier,
+        csn_url: process.env.ZAPIER_WEBHOOK_URL
+      })
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      console.error("Dopple error:", data);
+      return res.status(500).send("Unable to create finance application.");
+    }
+
+    const applicationUrl =
+      data.application_url ||
+      data.redirect_url ||
+      data.url ||
+      data.apply_url;
+
+    if (!applicationUrl) {
+      console.error("No application URL returned:", data);
+      return res.status(500).send("Finance application created but no redirect URL was returned.");
+    }
+
+    return res.redirect(applicationUrl);
+
+  } catch (error) {
+    console.error("Server error:", error);
+    return res.status(500).send("Something went wrong creating the finance application.");
+  }
+}
